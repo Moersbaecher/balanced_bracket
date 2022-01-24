a69f9a1593d4c19b1213fecf19a6cb865ad61fb3
Guilherme Moersbaecher <guipaes@hotmail.com>
2022-01-24 17:05:47 -0300

    segunda versão

--- bb.js
+++ bb.js
@@ -1,23 +1,28 @@
 let input = document.getElementById('input');
 let output = document.getElementById('output');
 let check = document.getElementById('check');
-const data = [input.value];
+const data = [];
 
 check.addEventListener('click', function(){
   
   let textField = input.value;
+  let message = undefined;
 
    for (let i = 0; i < textField.length; i++){
        if(textField[i] === "(" ){ 
             data.push( "(" );
-       } else if(textField[i] === ")" ){
+       } else if(textField[i] === ")" && data.length > 0){
             data.pop("(");
-       } else if(data.length = 0 ){
-          console.log("contou fechamento a mais");
+       } else if(textField[i] === ")" && data.length == 0 ){
+          message = "um ) ou mais";
      }
    } console.log(data);
 
-     output.innerHTML = data.length + '<br>' + data;
+   if(message){
+     output.innerHTML = message;
+   } else {
+        output.innerHTML = data.length + '<br>' + data;
+   }
 
      while(data.length > 0) {
           data.pop();
