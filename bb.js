6a7f0babaad3ad2b8842c6ee9c61f4a3b9eddbaf
Guilherme Moersbaecher <guipaes@hotmail.com>
2022-02-28 20:03:03 -0300

    funcao asterisco no output, ainda com problema

--- bb.js
+++ bb.js
@@ -1,46 +1,73 @@
+const data = [];
+const fechamento = [];
 let input = document.getElementById('input');
 let output = document.getElementById('output');
 let check = document.getElementById('check');
 let clear = document.getElementById('clear');
-const data = [];
-const fechamento = [];
+let base = [];
+let textField = input.value;
 
-check.addEventListener('click', function(){
-  
-  let textField = input.value;
-  
+check.addEventListener('click', function () { 
 
-   for (let i = 0; i < textField.length; i++){
-       if(textField[i] === "(" ){ 
-            data.push( "( na " + [i] + " posição" )
-            console.log('push');
-       } else if(textField[i] === ")" && data.length > 0){
-            data.pop("(")
-            console.log('pop');
-       } else if(textField[i] === ")" && data.length == 0 ){
-          fechamento.push(") na " + [i] + " posição")
-          console.log('sobrou' + [i]);
+     for (let i = 0; i < textField.length; i++) {
+          if (textField[i] === "(") {
+               data.push([i + 1]);
+          } else if (textField[i] === ")") {
+               if (data.length == 0) {
+                    fechamento.push([i + 1]);
+               } else if (data.length > 0) {
+                    data.pop([i + 1]);
+               }
+          }
      }
-     console.log([i])
-   } 
-   console.log(data);
-   console.log(fechamento);
-  // console.log(input.value);
 
-   if(fechamento.length > 0){
-     output.innerHTML = fechamento + "<br>" + data + "<br>"+ input.value;
-   } else {
-        output.innerHTML = data + "<br>" + fechamento + "<br>" + input.value;
-   }
+     console.log("data: " + data, isNaN(data));
+     console.log("fechamento: " + fechamento, isNaN(fechamento));
 
-     while(data.length > 0) {
+
+     base =[data, fechamento];  
+     base.sort();
+
+     asterisco(input.value);
+
+     while (data.length > 0) {
           data.pop();
-      };
+     };
+     while (fechamento.length > 0) {
+          fechamento.pop();
+     };
 
-     return data;
+     //return data;
 })
 
-clear.addEventListener('click', function(){
+
+
+clear.addEventListener('click', function () {
      input.value = "";
      data.pop();
+     fechamento.pop();
+     output.innerHTML = "";
 });
+
+
+//const inputStr = input.value.split(" ");
+const inputStr = Array.from(input.value);
+console.log('conteudo: ' + inputStr);
+console.log('quantidade: ' + inputStr.length);
+
+
+function asterisco(texto){
+
+     for (let i = 1; i <= texto.length; i++){
+
+          console.log("base: " + base + " and " + isNaN(base));
+          console.log("i: "+ i)
+
+            if(data == i || fechamento == i){
+               console.log("if")
+               console.log(texto.substring(0,i) + "*" + texto.substring(i, texto.length));
+          } else {
+               console.log("else")
+          } 
+     }
+}
